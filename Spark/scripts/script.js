30b5c817e1d91bc0cf7f40df423b183ed911e717
John <likisongproduct@gmail.com>
2020-06-07 00:15:06 -0700

    partial onclick animation

--- Spark/scripts/script.js
+++ Spark/scripts/script.js
@@ -2,48 +2,20 @@
 // from a quaternion-based representation of it. The constructed rotation is used with
 // AnimationModule.samplers.polyline to create a rotation animation that is expressed as a RotationSignal.
 
-var SceneModule = require('Scene');
-var AnimationModule = require('Animation');
-var ReactiveModule = require('Reactive');
+const SceneModule = require('Scene');
+const Animation = require('Animation');
 
-// Construct a Rotation object from a quaternion-based values.
-function axisRotation(axis_x, axis_y, axis_z, angle_degrees) {
-    var norm = Math.sqrt(axis_x*axis_x + axis_y*axis_y + axis_z*axis_z);
-    axis_x /= norm;
-    axis_y /= norm;
-    axis_z /= norm;
-    var angle_radians = angle_degrees * Math.PI / 180.0;
-    var cos = Math.cos(angle_radians/2);
-    var sin = Math.sin(angle_radians/2);
-    return ReactiveModule.rotation(
-        cos, axis_x*sin, axis_y*sin, axis_z*sin);
-}
-
-var time_driver = AnimationModule.timeDriver({
+const time_driver = Animation.timeDriver({
     durationMilliseconds: 2000,
     loopCount: Infinity,
-    mirror: true
+    mirror: false
 });
 
 // Create a rotation sampler using Rotation objects generated
 // by the previously-defined axisRotation() method.
-var rotation_sampler = AnimationModule.samplers.polyline({
-    keyframes: [
-        axisRotation(1,0,0,0),
-        axisRotation(1,0,0,45),
-        axisRotation(1,1,0,45),
-        axisRotation(1,0,1,45),
-        axisRotation(1,0,0,0)
-    ],
-    knots: [
-        0, 1, 2, 3, 4
-    ]
-});
+const rotation_sampler = Animation.samplers.linear(0, 1 * Math.PI);
+
+SceneModule.root.find("Seed").transform.rotationY = Animation.animate(time_driver, rotation_sampler);
 
 // Start the animation
-var rotation_signal = AnimationModule.animate(time_driver, rotation_sampler);
 time_driver.start();
-
-// Apply the rotation transform to a scene object.
-var plane = SceneModule.root.find('Seed');
-plane.transform.rotation = rotation_signal;
