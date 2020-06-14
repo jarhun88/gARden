a8a8cf07a590c79c5fc47a3a6f6fc893a7309643
john <likisongproduct@gmail.com>
2020-06-13 18:14:58 -0700

    added toggle with 3d objects

--- Spark/scripts/nativeui.js
+++ Spark/scripts/nativeui.js
@@ -8,32 +8,57 @@ Promise.all ([
     Textures.findFirst('pesticide'),
     Textures.findFirst('sun'),
 
-    Scene.root.findFirst('nullObject0'),
-    Scene.root.findFirst('nullObject1'),
+    Scene.root.findFirst('GasStation'),
+    Scene.root.findFirst('1220 Island'),
+    Scene.root.findFirst('Icosahedron'),
 
 ]).then(function(results){
 
-    const texture0 = results[0];
-    const texture1 = results[1];
-    const texture2 = results[2];
+    const button0 = results[0];
+    const button1 = results[1];
+    const button2 = results[2];
 
-    const Obj1 = results[2];
-    const Obj2 = results[3];
+    const obj0 = results[3];
+    const obj1 = results[4];
+    const obj2 = results[5];
 
     const configuration = {
 
         selectedIndex: 0,
 
         items: [
-            {image_texture: texture0},
-            {image_texture: texture1},
-            {image_texture: texture2},
+            {image_texture: button0},
+            {image_texture: button1},
+            {image_texture: button2},
         ]
     };
 
     const picker = NativeUI.picker;
     picker.configure(configuration);
     picker.visible = true;
+
+    picker.selectedIndex.monitor().subscribe(function(val) {
+        switch(val.newValue) {
+            case 0: {
+                obj0.hidden = false;
+                obj1.hidden = true;
+                obj2.hidden = true;
+                break;
+            }
+            case 1: {
+                obj0.hidden = true;
+                obj1.hidden = false;
+                obj2.hidden = true;
+                break;
+            }
+            case 2: {
+                obj0.hidden = true;
+                obj1.hidden = true;
+                obj2.hidden = false;
+                break;
+            }
+        }
+    });
 
 });
 
