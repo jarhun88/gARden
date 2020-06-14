98949680edd40c13a8db192c9698362e3892be2f
tsu019 <marisachanth@gmail.com>
2020-06-13 17:01:33 -0700

    added native ui picker

---
+++ Spark/scripts/nativeui.js
@@ -0,0 +1,36 @@
+const Scene = require('Scene');
+const NativeUI = require('NativeUI');
+const Textures = require('Textures');
+
+Promise.all ([
+
+    Textures.findFirst('water droplet'),
+    Textures.findFirst('pesticide'),
+
+    Scene.root.findFirst('nullObject0'),
+    Scene.root.findFirst('nullObject1'),
+
+]).then(function(results){
+
+    const texture0 = results[0];
+    const texture1 = results[1];
+
+    const Obj1 = results[2];
+    const Obj2 = results[3];
+
+    const configuration = {
+
+        selectedIndex: 0,
+
+        items: [
+            {image_texture: texture0},
+            {image_texture: texture1}
+        ]
+    };
+
+    const picker = NativeUI.picker;
+    picker.configure(configuration);
+    picker.visible = true;
+
+});
+
