724cddc62e33105e730403d6a972a4d466737ffe
tsu019 <marisachanth@gmail.com>
2020-06-13 17:18:34 -0700

    added 1 more ui picker

--- Spark/scripts/nativeui.js
+++ Spark/scripts/nativeui.js
@@ -6,6 +6,7 @@ Promise.all ([
 
     Textures.findFirst('water droplet'),
     Textures.findFirst('pesticide'),
+    Textures.findFirst('sun'),
 
     Scene.root.findFirst('nullObject0'),
     Scene.root.findFirst('nullObject1'),
@@ -14,6 +15,7 @@ Promise.all ([
 
     const texture0 = results[0];
     const texture1 = results[1];
+    const texture2 = results[2];
 
     const Obj1 = results[2];
     const Obj2 = results[3];
@@ -24,7 +26,8 @@ Promise.all ([
 
         items: [
             {image_texture: texture0},
-            {image_texture: texture1}
+            {image_texture: texture1},
+            {image_texture: texture2},
         ]
     };
 
