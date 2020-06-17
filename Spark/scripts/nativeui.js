dc9b19a741fec1db8db71735ef454ddc5c8145fe
Atif Mahmud <atifmahmud95@gmail.com>
2020-06-16 18:01:09 -0700

    spray can appears

--- Spark/scripts/nativeui.js
+++ Spark/scripts/nativeui.js
@@ -9,7 +9,7 @@ Promise.all ([
     Textures.findFirst('sun'),
 
     Scene.root.findFirst('watering can'),
-    Scene.root.findFirst('1220 Island'),
+    Scene.root.findFirst('can'),
     Scene.root.findFirst('Icosahedron'),
 
 ]).then(function(results){
