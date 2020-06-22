eab6f2d52b6085c3c3c272731f64f0bf9a3ded4f
tsu019 <marisachanth@gmail.com>
2020-06-21 20:59:40 -0700

    animated sun

--- Spark/scripts/nativeui.js
+++ Spark/scripts/nativeui.js
@@ -10,23 +10,21 @@ Promise.all ([
     Textures.findFirst('blank'),
     Textures.findFirst('water droplet'),
     Textures.findFirst('pesticide'),
-    Textures.findFirst('sun'),
+    
 
     Scene.root.findFirst('watering can'),
     Scene.root.findFirst('spray bottle'),
-    Scene.root.findFirst('sunlight'),
+    
 
 ]).then(function(results){
 
     const button0 = results[0];
     const button1 = results[1];
     const button2 = results[2];
-    const button3 = results[3];
-
-    const obj0 = results[4];
-    const obj1 = results[5];
-    const obj2 = results[6];
-
+    
+    const obj0 = results[3];
+    const obj1 = results[4];
+    
     const configuration = {
         selectedIndex: 0,
         items: [
@@ -37,7 +35,6 @@ Promise.all ([
 
     obj0.hidden = true;
     obj1.hidden = true;
-    obj2.hidden = true;
 
     const picker = NativeUI.picker;
     picker.configure(configuration);
@@ -51,19 +48,13 @@ Promise.all ([
             case 1: {
                 obj0.hidden = false;
                 obj1.hidden = true;
-                obj2.hidden = true;
+                
                 break;
             }
             case 2: {
                 obj0.hidden = true;
                 obj1.hidden = false;
-                obj2.hidden = true;
-                break;
-            }
-            case 3: {
-                obj0.hidden = true;
-                obj1.hidden = true;
-                obj2.hidden = false;
+                
                 break;
             }
         }
